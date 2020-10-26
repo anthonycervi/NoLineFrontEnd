1dcf5fc8f8b13503a2ae0e2f56710168960ddc72
Josh Glacier <josh_mountain@live.com>
2020-10-26 14:47:16 -0700

    Last 2 Components, Only polishing remains

--- stories/JoshComps.stories.js
+++ stories/JoshComps.stories.js
@@ -2,11 +2,13 @@ import React from 'react';
 import InputComp from '../comps/InputComp'
 import SearchTitle from '../comps/SearchTitle'
 import UserPage from '../comps/UserPage'
+import MapOverlay from '../comps/MapOverlay'
+import SearchResult from '../comps/SearchResult'
 
 
 export default {
     title: 'Example/FormButtons',
-    component: InputCompForm, SearchTitleTop,UserPageTop
+    component: InputCompForm, SearchTitleTop,UserPageTop,GoogleMapOverlay,SearchResultOverlay
 }
 
 
@@ -16,3 +18,7 @@ export const InputCompForm = () => <InputComp/>
 export const SearchTitleTop = () => <SearchTitle/>
 
 export const UserPageTop = () => <UserPage/>
+
+export const GoogleMapOverlay = () => <MapOverlay/>
+
+export const SearchResultOverlay = () => <SearchResult/>

6ba3d066e34fe3b6faa0696c852c41915d60c1c6
Josh Glacier <josh_mountain@live.com>
2020-10-26 00:15:05 -0700

    Form Comp with Props Integrated

---
+++ stories/JoshComps.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import InputComp from '../comps/InputComp'
+
+
+
+export default {
+    title: 'Example/FormButtons',
+    component: InputCompForm
+}
+
+
+
+export const InputCompForm = () => <InputComp/> 

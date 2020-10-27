499d718365fde3ffc7526133e3b24bd43805d0c3
anthonycervi <anthonycervi97gmail.com>
2020-10-27 13:21:32 -0700

    little change

--- comps/AccountText/index.js
+++ comps/AccountText/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import styled from 'styled-components/native';
 import {View, Text} from "react-native";
 
+const TextAlt = styled.Text`
+font-weight:700;
+`;
 
-
-const AccountTextContainer = styled.p`
+const AccountTextContainer = styled.Text`
     position:relative;
     display: flex;
     flex-direction: row;
@@ -16,15 +18,15 @@ const AccountTextContainer = styled.p`
     font-style: normal;
     font-size: 11px;
     color: #B0B0B0;
-    span {
-        font-weight:700;
-    }
      `;
 
- const AccountText = ({text, text2, span}) => {
-       return <View><AccountTextContainer style ={{text, span}}>
-                {text} <span>{text2}</span>
-              </AccountTextContainer></View>
+ const AccountText = ({text, text2}) => {
+       return <View>
+                <AccountTextContainer>
+                {text} 
+              </AccountTextContainer>
+                <TextAlt>{text2}</TextAlt>
+              </View>
     
     }
 

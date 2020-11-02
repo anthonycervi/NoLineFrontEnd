6bc24c88136687be6fd8de2796c90927d73b6036
Josh Glacier <josh_mountain@live.com>
2020-11-02 13:58:55 -0800

    Removed Node

---
+++ NoLine/comps/InputComp/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const FormStyle = styled.label`
+
+`;
+const FormLabel = styled.label`
+
+color: ${props => props.color ? props.color : "#686868" };
+position:relative;
+left:.5em;
+`;
+
+const FormInput = styled.input`
+width: ${props => props.width ? props.width :"384"};
+height: ${props => props.height ? props.height :"45"};
+margin:.5em;
+border-radius:3.75%;
+
+`;
+
+const InputComp = ({text,color,width,height}) => {
+   return <FormStyle>
+    
+  <FormLabel color={color}>{text}</FormLabel><br/> 
+  <FormInput width={width} height={height}/> <br/>
+   </FormStyle>
+
+}
+
+InputComp.defaultProps= {
+   text:"Default",
+   color:"#686868",
+   width:"384px",
+   height:"45px"
+ }
+
+export default InputComp

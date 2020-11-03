13c64c8b30424f8f9c25a4db698bc1c169fedbfb
anthonycervi <anthonycervi97gmail.com>
2020-11-03 11:53:18 -0800

    made changes

---
+++ NoLine/comps/Filter/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import {View} from "react-native";
+
+const FilterContainer = styled.View`
+    width: 21px;
+    height: 21px;
+    background-color: #EBEBEB;
+    position:relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: url(/Filter.svg); 
+    background-repeat: no-repeat;
+     `;
+
+
+
+ const Filter = ({}) => {
+       return <View><FilterContainer style ={{}}>
+              </FilterContainer></View>
+    
+    }
+
+    Filter.defaultProps= {}
+
+export default Filter;
---
+++ NoLine/storybook/stories/Filter.stories.js
@@ -0,0 +1,19 @@
+import { storiesOf } from '@storybook/react-native';
+import React from 'react';
+import Filter from '../comps/Filter';
+
+
+
+storiesOf('Filter', module)
+.add('DefaultFilter', () => (
+<Filter />
+))
+
+// export default {
+//     title: 'Example/Filter',
+//     component: Filter
+// }
+
+
+
+// export const DefaultFilter = () => <Filter/>

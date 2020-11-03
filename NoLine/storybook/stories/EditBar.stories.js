6d9f476e4b6bdf7d85c37f2e7e455d92596295a1
Farhaz Ladha <farhaz@iMac.hitronhub.home>
2020-11-03 12:05:34 -0800

    Adding comps

--- NoLine/storybook/stories/EditBar.stories.js
+++ NoLine/storybook/stories/EditBar.stories.js
@@ -1,9 +1,28 @@
 import { storiesOf } from '@storybook/react-native';
 import React from 'react';
+import CenterView from './CenterView';
 import EditBar from '../../comps/EditBar';
+import Profile from '../../comps/Profile';
+import InputIconBar from '../../comps/InputIconBar';
+// import Message from '../../comps/Message';
 
 storiesOf('EditBar', module)
+.addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
   .add('Basic EditBar', () => (
     <EditBar />
   ))
+  .add('Profile', () => (
+    <Profile />
+  ))
+  .add('IconBar', () => (
+    <InputIconBar />
+  ))
+  // .add('Message', () => (
+  //   <Message />
+  // ))
+
+
+  // storiesOf('InputIconBar', module).add('Basic InputIconBar', () => (
+  //   <InputIconBar />
+  // ))
   //.add() more stories

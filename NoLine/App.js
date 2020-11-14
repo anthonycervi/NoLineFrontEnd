1b8e48b93434831b1ddecc245ffdefa307867182
Josh Glacier <josh_mountain@live.com>
2020-11-14 14:31:24 -0800

    Page Polishing

--- NoLine/App.js
+++ NoLine/App.js
@@ -15,31 +15,13 @@ import {
   Text,
   StatusBar,
 } from 'react-native';
+import SearchResult from './comps/SearchResult';
 
-import InputIconBar from './comps/InputIconBar';
-import Button from './comps/Button';
-import SearchResult from './comps/SearchResult'
-import Navigator from './comps/Navigator'
+import SearchResultPage from './pages/SearchResults'
 
 const App = () =>{
   return <View style={styles.cont}>
-    <View style={SearchInput.cont}>
-<InputIconBar></InputIconBar>
-</View>
-<View style={ButtonStyles.cont}>
-      <Button text="Wait Time" buttonborder={"3px solid #FFD25B"} buttonwidth={100} buttonheight={32}></Button> 
-      <Button text="Distance" buttonbgcolor="white" buttonborder buttonwidth={100} buttonheight={32} ></Button> 
-      </View>
-      <SearchResult></SearchResult>
-      <SearchResult></SearchResult>
-      <SearchResult></SearchResult>
-      <SearchResult></SearchResult>
-      
-      
-     
-
-      
-      <Navigator></Navigator>
+ <SearchResultPage></SearchResultPage>
   </View>
 }
 
@@ -50,24 +32,6 @@ cont:{
 }
 })
 
-const SearchInput = StyleSheet.create({
-  cont:{
-    flex:1,
-  justifyContent:"center",
-  left:20
-  }
-})
-
-const SearchResultStyle = StyleSheet.create({
-
-})
-
-const ButtonStyles = StyleSheet.create({
-  cont:{
-    flexDirection:"row",
-  margin:10
-  }
-})
 
 export default App;
 //export {default} from './storybook';
--- NoLine/pages/SearchResults.js
+++ NoLine/pages/SearchResults.js
@@ -15,16 +15,59 @@ import {
   Text,
   StatusBar,
 } from 'react-native';
-import Button from '../comps/Button';
-import SearchResult from '../comps/SearchResult';
-import Filter from '../comps/Filter';
- //SEARCH COMPS
-//OTHER COMPS ONCE TEAM HAS FINISHED
-const SearchResults = () =>{
-  return <View>
-    < Search/>
+
+import InputIconBar from './comps/InputIconBar';
+import Button from './comps/Button';
+import SearchResult from './comps/SearchResult'
+import Navigator from './comps/Navigator'
+
+const SearchResultPage = () =>{
+  return <View style={styles.cont}>
+    <View style={SearchInput.cont}>
+<InputIconBar></InputIconBar>
+</View>
+<View style={ButtonStyles.cont}>
+      <Button text="Wait Time" buttonborder={"3px solid #FFD25B"} buttonwidth={100} buttonheight={32}></Button> 
+      <Button text="Distance" buttonbgcolor="white" buttonborder buttonwidth={100} buttonheight={32} ></Button> 
+      </View>
+      <SearchResult></SearchResult>
+      <SearchResult></SearchResult>
+      <SearchResult></SearchResult>
+      <SearchResult></SearchResult>
+      
+      
+     
+
+      
+      <Navigator></Navigator>
   </View>
 }
 
-export default SearchResults;
-//export {default} from './storybook'; 
+const styles = StyleSheet.create({
+cont:{
+  flex:1,
+  justifyContent:"center",
+}
+})
+
+const SearchInput = StyleSheet.create({
+  cont:{
+    flex:1,
+  justifyContent:"center",
+  left:20
+  }
+})
+
+const SearchResultStyle = StyleSheet.create({
+
+})
+
+const ButtonStyles = StyleSheet.create({
+  cont:{
+    flexDirection:"row",
+  margin:10
+  }
+})
+
+export default SearchResultPage;
+//export {default} from './storybook';

cd7bd46f32196dade7889b059b420c69f36dbce7
Josh Glacier <josh_mountain@live.com>
2020-11-14 17:26:37 -0800

    Basic Routing V3

--- NoLine/App.js
+++ NoLine/App.js
@@ -15,23 +15,60 @@ import {
   Text,
   StatusBar,
 } from 'react-native';
-import SearchResult from './comps/SearchResult';
 
-import SearchResultPage from './pages/SearchResults'
+import SearchTitle from './comps/SearchTitle';
+import Slider from './comps/Slider';
+import Button from './comps/Button';
+import Navigator from './comps/Navigator';
 
+
+//MAP COMPONENT IS ADRIAN'S GOOGLE MAP API
 const App = () =>{
   return <View style={styles.cont}>
- <SearchResultPage></SearchResultPage>
+ <SearchTitle style={title.cont}></SearchTitle>
+
+<View style={ButtonStyle.cont}>
+ <Button text="Submit" buttoncolor="#FFFFFF" buttonborder={"3px solid #FFD25B"} buttonwidth={135} buttonheight={35}></Button> 
+ </View>
+
+
+
+<View style={Nav.cont}>
+<Navigator></Navigator>
+</View>
   </View>
 }
 
 const styles = StyleSheet.create({
 cont:{
-  flex:1,
-  justifyContent:"center",
+  
 }
 })
 
+const title= StyleSheet.create({
+  cont:{
+    position:"relative",
+    top:100
+  }
+})
+
+const Nav = StyleSheet.create({
+  cont:{
+    position:"relative",
+    top:520
+  }
+})
+
+const ButtonStyle = StyleSheet.create({
+  cont:{
+    position:"relative",
+    left:120,
+    top:300,
+    flex:1,
+    justifyContent:"center",
+    alignContent:"center"
+  }
+})
 
 export default App;
 //export {default} from './storybook';
